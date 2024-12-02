cd06c29df91f1e6f4eb4f8b535e36ae1550323cc
Mohamed2222106 <mo.waaled@gmail.com>
2024-12-02 04:44:06 +0200

    Delete 2script.js

--- 2script.js
+++
@@ -1,7 +0,0 @@
-function openMenu(tableId, event) {
-  if (event.target.tagName === "BUTTON") return;
-  activeTableId = tableId;
-  document.getElementById("menu-popup").style.display = "block";
-}
-
-tableDiv.addEventListener("click", (event) => openMenu(table.id, event));
